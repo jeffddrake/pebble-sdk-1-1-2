5eab5c2fa528ce545ce314edf814da6946b81eec
jeffddrake <jeff.d.drake@gmail.com>
2013-11-03 17:18:55 -0500

    - changed to Jeff Drake inc.

--- PebbleSDK-1.12/notify_mute/src/notify_mute.c
+++ PebbleSDK-1.12/notify_mute/src/notify_mute.c
@@ -5,7 +5,7 @@
 
 #define MY_UUID { 0x2A, 0x2C, 0x1C, 0x86, 0x61, 0xD4, 0x47, 0x23, 0xBB, 0x4F, 0x58, 0x45, 0x49, 0xD4, 0xD6, 0x55 }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
+             "Template App", "Jeff Drake inc.",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_STANDARD_APP);
